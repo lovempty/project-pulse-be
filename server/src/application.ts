d07c33354649ad38e52f97173ccca01c7edac536
Synth <hopninhquang98@gmail.com>
2026-08-19 07:30:44 +0700

    check

--- server/src/application.ts
+++ server/src/application.ts
@@ -1,22 +1,25 @@
-import Fastify from 'fastify';
+import Fastify, { type FastifyInstance } from 'fastify';
 import core from './plugins/core.js';
 import routes from './routes.js';
 import { env } from './config/env.js';
 
-export function buildApp(options = {}) {
+export function createAppOptions(options = {}) {
   const logger = env.nodeEnv === 'test' ? false : env.nodeEnv === 'development'
     ? { level: 'debug', transport: { target: 'pino-pretty' } }
     : { level: 'info' };
-  const app = Fastify({
+  return {
     logger,
     ajv: { customOptions: { removeAdditional: false, coerceTypes: true, allErrors: true } },
     ...options
-  });
+  };
+}
+
+export function registerApplication(app: FastifyInstance) {
   app.register(core);
   app.register(routes);
   return app;
 }
 
-const app = buildApp();
-
-export default app;
+export function buildApp(options = {}) {
+  return registerApplication(Fastify(createAppOptions(options)));
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
-import app from './application.js';
+import Fastify from 'fastify';
+import { createAppOptions, registerApplication } from './application.js';
 import { env } from './config/env.js';
 
+const app = registerApplication(Fastify(createAppOptions()));
 const shutdown = async (signal: string) => { app.log.info({ signal }, 'shutting down'); await app.close(); process.exit(0); };
 process.on('SIGINT', () => void shutdown('SIGINT'));
 process.on('SIGTERM', () => void shutdown('SIGTERM'));
